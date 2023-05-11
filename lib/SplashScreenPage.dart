fcccd4a39ea8f02fd90262901f584d9dd34a0825
Quân <quanminhfc@gmail.com>
2023-05-11 18:39:31 +0700

    ok

--- lib/SplashScreenPage.dart
+++ lib/SplashScreenPage.dart
@@ -1,25 +1,45 @@
+import 'dart:async';
+
 import 'package:demo_webview/main.dart';
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 import 'package:lottie/lottie.dart';
 
-class SplashScreenPage extends StatelessWidget {
+class SplashScreenPage extends StatefulWidget {
   const SplashScreenPage({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreenPage> createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) => MyHomePage()));
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    return SplashScreen(
-      navigateAfterSeconds: const MyHomePage(),
-      seconds: 3,
-      title: const Text('Welcome In SplashScreen',
-        style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20.0
-        ),),
-        backgroundColor: Colors.white,
-        styleTextUnderTheLoader: TextStyle(),
-        photoSize: 100.0,
-        loaderColor: Colors.blueAccent
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+                child:
+                    // Lottie.network(
+                    //     'https://assets5.lottiefiles.com/packages/lf20_7HVK9ugFmX.json'),
+                    Lottie.asset(
+              'assets/animation/ok.json',
+            )),
+          ],
+        ),
+      ),
     );
+    ;
   }
 }
